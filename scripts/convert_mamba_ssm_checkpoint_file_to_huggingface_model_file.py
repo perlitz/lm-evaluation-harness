6542d015a47d40b6184c061f0defef58654fc569
Yotam Perlitz <perlitz@gmail.com>
2025-03-20 10:22:38 +0100

    Update convert_mamba_ssm_checkpoint_file_to_huggingface_model_file.py

--- scripts/convert_mamba_ssm_checkpoint_file_to_huggingface_model_file.py
+++ scripts/convert_mamba_ssm_checkpoint_file_to_huggingface_model_file.py
@@ -2,7 +2,6 @@ import argparse
 import os
 
 from loguru import logger
-from tqdm import tqdm
 from transformers.models.bamba.convert_mamba_ssm_checkpoint import (
     convert_mamba_ssm_checkpoint_file_to_huggingface_model_file,
 )
@@ -25,24 +24,24 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    for input_model_path in tqdm(args.input_model_paths, desc="Converting models"):
-        tokenizer_files = [f for f in os.listdir(input_model_path) if "tok" in f]
-        if len(tokenizer_files) == 0:
-            logger.info(f"No tokenizer files found for {input_model_path}, skipping")
-            continue
+    input_model_path = args.input_model_path
+    tokenizer_files = [f for f in os.listdir(input_model_path) if "tok" in f]
+    if len(tokenizer_files) == 0:
+        logger.info(f"No tokenizer files found for {input_model_path}, skipping")
+        raise ValueError(f"No tokenizer files found for {input_model_path}, skipping")
 
-        out_model_dir_path = args.model_save_dir + input_model_path + "-hf"
-        convert_mamba_ssm_checkpoint_file_to_huggingface_model_file(
-            input_model_path,
-            "fp16",
-            out_model_dir_path,
-            save_model="sharded",
-            tokenizer_path=input_model_path,
+    out_model_dir_path = args.model_save_dir + input_model_path + "-hf"
+    convert_mamba_ssm_checkpoint_file_to_huggingface_model_file(
+        input_model_path,
+        "fp16",
+        out_model_dir_path,
+        save_model="sharded",
+        tokenizer_path=input_model_path,
+    )
+    import shutil
+
+    for tokenizer_file in tokenizer_files:
+        shutil.copyfile(
+            src=os.path.join(input_model_path, tokenizer_file),
+            dst=os.path.join(out_model_dir_path, tokenizer_file),
         )
-        import shutil
-
-        for tokenizer_file in tokenizer_files:
-            shutil.copyfile(
-                src=os.path.join(input_model_path, tokenizer_file),
-                dst=os.path.join(out_model_dir_path, tokenizer_file),
-            )
